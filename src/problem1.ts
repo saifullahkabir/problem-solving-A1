ce9b4003de6c9b88b7285333d3baa48c1244027f
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-11-16 23:14:32 +0600

    implement generic and type assertion for return type checking

--- src/problem1.ts
+++ src/problem1.ts
@@ -1,15 +1,15 @@
 type Value = string | number | boolean;
 
-const formatValue = (value: Value) => {
+const formatValue = <T extends Value>(value: T): T => {
   if (typeof value === "string") {
-    return value.toUpperCase();
+    return value.toUpperCase() as T;
   } else if (typeof value === "number") {
-    return value * 10;
+    return (value * 10) as T;
   } else {
-    return !value;
+    return !value as T;
   }
 };
 
-console.log(formatValue('hello'));
+console.log(formatValue("hello"));
 console.log(formatValue(5));
 console.log(formatValue(true));
