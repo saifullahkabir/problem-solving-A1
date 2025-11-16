e6b1f0d5a03378aa0e1dd9221a82c1fbe34f61d9
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-11-16 22:35:13 +0600

    completely solve problem 1

---
+++ dist/problem1.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const userList = { name: 'rana', age: 23 };
+console.log(userList);
+//# sourceMappingURL=problem1.js.map
