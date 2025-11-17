771cd1b9787bc8ea97856b2a562490938a761e20
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-11-17 19:49:12 +0600

    solved problem 6

---
+++ src/problem6.ts
@@ -0,0 +1,24 @@
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+const printBookDetails = (book: Book) => {
+  const { title, author, publishedYear, isAvailable } = book;
+  console.log(
+    `Title: ${title}, Author: ${author}, Published: ${publishedYear}, Available: ${
+      isAvailable ? "Yes" : "No"
+    }`
+  );
+};
+
+const myBook: Book = {
+  title: "The Great Gatsby",
+  author: "F. Scott Fitzgerald",
+  publishedYear: 1925,
+  isAvailable: true,
+};
+
+printBookDetails(myBook);
