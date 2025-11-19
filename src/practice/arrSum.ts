15c98cdc6f670b6ea010bf08d28105534d30f02a
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-11-20 03:06:42 +0600

    use forEach, for loop for array sum

--- src/practice/arrSum.ts
+++ src/practice/arrSum.ts
@@ -3,9 +3,27 @@
 
 //* sum([1, 2, 3, 4]) // 10
 
-
 const sum = (arr: number[]) : number => {
     return arr.reduce((acc, current) => acc + current, 0);
 }
 
 console.log(sum([1, 2, 3, 4]));  // 10
+
+//* using forEach
+// const sum = (arr: number[]): number => {
+//   let total = 0;
+//   arr.forEach((num) => {
+//     return (total += num);
+//   });
+//   return total;
+// };
+
+
+//* using for loop
+// const sum = (arr: number[]): number => {
+//   let total: number = 0;
+//   for (let i: number = 0; i < arr.length; i++) {
+//     total += arr[i] ?? 0;
+//   }
+//   return total;
+// };
